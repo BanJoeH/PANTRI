b56c2b84546b2e17d1659e5230a780e8eae973d6
DianomiJH <joe.harrison@dianomi.com>
2021-08-20 13:38:08 +0100

    BREAKING CHANGE: breaking change test

--- src/App/app.utils.test.js
+++ src/App/app.utils.test.js
@@ -42,6 +42,6 @@ describe("filterRecipeOut", () => {
   });
 
   it("returns full recipeList if not found", () => {
-    expect(utils.filterRecipeOut("alkjshdl", recipeList)).toEqual(recipeList);
+    expect(utils.filterRecipeOut("alkjsshdl", recipeList)).toEqual(recipeList);
   });
 });
